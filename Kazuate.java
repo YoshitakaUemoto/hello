1fe5aeb9ebe2d2067fb714e8942be3da9070617c
YoshitakaUemoto <210x204x@stu.kobe-u.ac.jp>
2021-06-15 11:15:12 +0900

    java作成

---
+++ Kazuate.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Kazuate{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int ans = 39;
+        int num;
+
+        System.out.println("2桁の整数を入力してください");
+        for(int i=0;i<5;i++){
+            num = scanner.nextInt();
+            if(ans == num){
+                System.out.println("正解です.");
+                break;
+            }else{
+                System.out.println("違います。再入力してください。");
+                if(Math.abs(num-ans)>=20){
+                    System.out.println("正解と20以上離れています");
+                }
+            }
+
+            if(i == 4){
+                System.out.println("5回以内に成功しませんでした.");
+            }
+        }
+        
+    }
+}
